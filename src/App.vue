f153e8ec801f07cc1b93bc0f3c37cf6bdfbbe58e
Eric Castellanos <eric@ericcastellanos.com>
2021-04-30 05:26:10 -0500

    login message

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <NavBar/>
-  <Home/>
+  <Home v-if="isAuthenticated"/>
+  <notSignedin v-else/>
   <Footer/>
   </template>
   
   <script setup>
-  
+  import {authentication} from '~/functions/useFirebase'
+  const {isAuthenticated} = authentication()
   </script>
   
   
